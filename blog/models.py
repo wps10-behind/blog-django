779ef985a234e6877604731e92e761f55cfea40e
hongmin0907 <ghdals0907@naver.com>
2019-05-28 11:26:59 +0900

    first upload

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from ckeditor.fields import RichTextField
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30, db_index=True, unique=True, allow_unicode=True, blank=True)
+
+    class Meta:
+        ordering = ['slug']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                null=True, blank=True, related_name='posts')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=50)
+    text = RichTextField()
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
