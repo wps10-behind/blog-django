c68522a119e07b8e08e4dd389694023490f5bd6e
JINUKK <askjw123@naver.com>
2019-05-29 16:15:18 +0900

    update base templates and social login naver

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook'
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.naver',
 ]
 
 MIDDLEWARE = [
